ef6901de6524683c182aabc6a0bb3472c152a821
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2023-11-14 20:17:10 -0800

    Init SvelteKit app with Tailwind

---
+++ README.md
@@ -0,0 +1,9 @@
+# PO
+
+```shell
+# init sveltekit project
+npm create svelte@latest my-app
+
+# add tailwind
+
+```
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import "../app.css";
+</script>
+<div>
+  <h1>Layout</h1>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/contact">Contact</a>
+</div>
+
+<slot />
